e9c8eaecc30d43b77802fe6f79fbd73f06895322
姚欢欢 <yaohuanhuan@eqxiu.com>
2019-12-29 08:47:33 +0800

    xinde

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import './index.css';
-import TodoList from './TodoList';
+// import TodoList from './TodoList';
 
-ReactDOM.render(<TodoList />, document.getElementById('root'));
+ReactDOM.render(<h1>hello world</h1>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
